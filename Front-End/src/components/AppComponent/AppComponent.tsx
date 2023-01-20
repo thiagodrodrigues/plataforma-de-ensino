11e86906d253f3435d2fcf255d4bbd7fa86d506f
Thiago Rodrigues <thiago.alvimrodrigues@gmail.com>
2023-01-20 15:58:54 -0300

    Configurações iniciais de projeto

---
+++ Front-End/src/components/AppComponent/AppComponent.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+
+const AppComponent = () => {
+
+  return (
+    <>
+      <h1>Hello World!!</h1>
+    </>
+  )
+}
+
+export default AppComponent
---
+++ Front-End/src/pages/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AppComponent from '../components/AppComponent/AppComponent'
+
+function App () {
+    return (
+        <AppComponent />
+    )
+}
+
+export default App
